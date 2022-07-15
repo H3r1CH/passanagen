a1f69d90b7694aaebbf8cda9cdd01a6596806c04
H3r1CH <H3r1CH@protonmail.com>
2022-07-15 13:51:02 -0400

    Update some generate password code

--- passanagen/main.py
+++ passanagen/main.py
@@ -1,11 +1,21 @@
+from os import TMP_MAX
 import typer
+import random
+import string
 
 letters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 numbers = ('0','1','2','3','4','5','6','7','8','9')
 spec_chars = ('`','~','!','@','#','$','%','^','&','*','(',')','-','+','=','_','{','}','[',']','\\','|','/','?','>','<')
 length = ''
 
+lcl = string.ascii_lowercase
+ucl = string.ascii_uppercase
+num = string.digits
+char = string.punctuation
+
+
 app = typer.Typer(help="CLI Password Analyzer and Generator.")
+
 
 @app.command(help="Analyze a given password.")
 def analyze(password: str):
@@ -52,8 +62,56 @@ def analyze(password: str):
 
 
 @app.command(help="Generate a password based on selected properties.")
-def generate(password: str):
-    print(f"This is the {password}")
+def generate():
+    pass_length = typer.prompt("Select Password length")
+    pass_length_num = int(pass_length)
+    print(f"Password Length = {pass_length_num}")
+    upper_case = typer.confirm("Do you want upper case letters")
+    a_number = typer.confirm("Do you want any numbers")
+    a_char = typer.confirm("Do you want special characters")
+
+    initial_password = ''.join(random.choices(lcl, k=pass_length_num))
+    print(initial_password)
+
+    if upper_case:
+        upper_password = ''.join(random.choice((str.upper, str.lower))(u) for u in initial_password)
+        print(upper_password)
+        if a_number:
+            num_password = upper_password.replace(random.choice(upper_password), random.choice(num))
+            print(num_password)
+            if a_char:
+                char_password = num_password.replace(random.choice(num_password), random.choice(char))
+                print(char_password)
+            else:
+                print('no char')
+        else:
+            print('no number')
+
+    if a_number:
+        num_password = initial_password.replace(random.choice(initial_password), random.choice(num))
+        print(num_password)
+        if a_char:
+            char_password = num_password.replace(random.choice(num_password), random.choice(char))
+            print(char_password)
+        else:
+            print('no char')
+
+    
+    # else:
+    #     print(initial_password)  
+
+    if a_number:
+        print("a number from here")
+    #     num_password = initial_password.replace(random.choice(initial_password), random.choice(num))
+    #     print(num_password)
+    # else:
+    #     print(initial_password)
+
+
+    #print(password)    
+
+    # random.shuffle(password)
+    #print('You have generated the password:', "".join(password))
 
 
 if __name__ == "__main__":
