8f7858f019c045070eaf0fadca50ada273998ea3
H3r1CH <H3r1CH@protonmail.com>
2022-08-12 18:28:15 -0400

    Update generator, README, and version info

--- README.md
+++ README.md
@@ -5,69 +5,88 @@ CLI Password Analyzer and Generator
 ```bash
 pip install https://github.com/H3r1CH/passanagen/releases/download/v0.1.0/passanagen-0.1.0-py3-none-any.whl
 ```
-or
-```bash
-git clone https://github.com/H3r1CH/passanagen.git
-cd passanagen
-poetry install
-poetry shell
-```
-or
-```bash
-git clone https://github.com/H3r1CH/passanagen.git
-cd passanagen
-poetry install
-poetry build
-pip install dist/passanagen-0.1.0-py3-none-any.whl
-```
 
 ### Help
 ```bash
-passanagen --help        
-Usage: passanagen [OPTIONS] COMMAND [ARGS]...
+passanagen --help
 
-Options:
-  --install-completion  Install completion for the current shell.
-  --show-completion     Show completion for the current shell, to copy it or
-                        customize the installation.
-  --help                Show this message and exit.
+ Usage: passanagen [OPTIONS] COMMAND [ARGS]...
 
-Commands:
-  analyze
-  generate
+ CLI Password Analyzer and Generator.
+
+┌─ Options ──────────────────────────────────────────────────────────────────────────────────────────────────────┐
+│ --install-completion          Install completion for the current shell.                                        │
+│ --show-completion             Show completion for the current shell, to copy it or customize the installation. │
+│ --help                        Show this message and exit.                                                      │
+└────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+┌─ Commands ─────────────────────────────────────────────────────────────────────────────────────────────────────┐
+│ ana       Analyze a given password.                                                                            │
+│ gen       Generate a password based on selected properties.                                                    │
+└────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 ```
 
 ## Analyzer
 
 ### Help
 ```bash
-passanagen analyze --help
-Usage: passanagen analyze [OPTIONS] PASSWORD
+C:\Users\esanderford>passanagen ana --help
 
-Arguments:
-  PASSWORD  [required]
+ Usage: passanagen ana [OPTIONS] PASSWORD
+
+ Analyze a given password.
+
+┌─ Arguments ─────────────────────────────────────────┐
+│ *    password      TEXT  [default: None] [required] │
+└─────────────────────────────────────────────────────┘
+┌─ Options ───────────────────────────────────────────┐
+│ --help          Show this message and exit.         │
+└─────────────────────────────────────────────────────┘
 ```
 
 ### Example
 ```bash
-passanagen analyze password123 
+passanagen ana password123
 ----------------------
-PASSWORD DETAILS
+ANALYZED PASSWORD
 ----------------------
 > Password Length:  11
 > Uppercase Letters: 0
 > Lowercase Letters: 8
 > Number count: 3
 > Special Character count: 0
-----------------------
-PASSWORD STRENGTH
-----------------------
+
 [-] Your password "password123" contains NO uppercase!
 [-] Your password "password123" contains NO special characters!
+----------------------
 ```
 
 ## Generator
 
 ### Help
+```bash
+passanagen gen --help
+
+ Usage: passanagen gen [OPTIONS]
+
+ Generate a password based on selected properties.
+
+┌─ Options ────────────────────────────────────┐
+│ --help          Show this message and exit.  │
+└──────────────────────────────────────────────┘
+```
 
 ### Example
+```bash
+passanagen gen
+Select Password length: 11
+Password Length = 11
+Do you want lower case letters? [y/N]: y
+Do you want upper case letters? [y/N]: y
+Do you want any numbers? [y/N]: y
+Do you want special characters? [y/N]: y
+----------------------
+GENERATED PASSWORD
+----------------------
+[+] Your new password is: ;8QBD[H9f/~
+----------------------
+```
--- passanagen/main.py
+++ passanagen/main.py
@@ -1,12 +1,9 @@
+from mimetypes import init
+from multiprocessing.sharedctypes import Value
 from os import TMP_MAX
 import typer
 import random
 import string
-
-letters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
-numbers = ('0','1','2','3','4','5','6','7','8','9')
-spec_chars = ('`','~','!','@','#','$','%','^','&','*','(',')','-','+','=','_','{','}','[',']','\\','|','/','?','>','<')
-length = ''
 
 lcl = string.ascii_lowercase
 ucl = string.ascii_uppercase
@@ -17,10 +14,10 @@ char = string.punctuation
 app = typer.Typer(help="CLI Password Analyzer and Generator.")
 
 
-@app.command(help="Analyze a given password.")
+@app.command("ana", help="Analyze a given password.")
 def analyze(password: str):
     print('-' * 22)
-    print('PASSWORD DETAILS')
+    print('ANALYZED PASSWORD')
     print('-' * 22)
     print('> Password Length: ', len(password))
     upper_count = 0
@@ -32,20 +29,18 @@ def analyze(password: str):
             upper_count += 1
         elif u.islower():
             lower_count += 1
-        elif u in numbers:
+        elif u in num:
             num_count += 1
-        elif u in spec_chars:
+        elif u in char:
             char_count += 1
         else:
-            print('something else')          
+            print('[-] Unknown character!')          
     print('> Uppercase Letters:', upper_count)
     print('> Lowercase Letters:', lower_count)
     print('> Number count:', num_count)
     print('> Special Character count:', char_count)
-
-    print('-' * 22)
-    print('PASSWORD STRENGTH')
-    print('-' * 22)
+    print()
+    
     if len(password) < 8:
         print(f'[-] Your password "{password}" is too short! Use a minimum length of 8!')
     if upper_count < 1:
@@ -61,57 +56,56 @@ def analyze(password: str):
     print('-' * 22)
 
 
-@app.command(help="Generate a password based on selected properties.")
+@app.command("gen", help="Generate a password based on selected properties.")
 def generate():
     pass_length = typer.prompt("Select Password length")
     pass_length_num = int(pass_length)
-    print(f"Password Length = {pass_length_num}")
-    upper_case = typer.confirm("Do you want upper case letters")
-    a_number = typer.confirm("Do you want any numbers")
-    a_char = typer.confirm("Do you want special characters")
+    if pass_length_num < 8:
+        print("Password too short. Please use at least 8 characters.")
+        generate()
+    else:
+        print(f"Password Length = {pass_length_num}")
+        low_case = typer.confirm("Do you want lower case letters?")
+        up_case = typer.confirm("Do you want upper case letters?")
+        a_number = typer.confirm("Do you want any numbers?")
+        a_char = typer.confirm("Do you want special characters?")
 
-    initial_password = ''.join(random.choices(lcl, k=pass_length_num))
-    print(initial_password)
+        initial_password = ''
+        unique_case = 0
+        combined = ''
+        remaining_password = ''
 
-    if upper_case:
-        upper_password = ''.join(random.choice((str.upper, str.lower))(u) for u in initial_password)
-        print(upper_password)
+        if low_case:
+            lower_password = random.choice(lcl)
+            initial_password += lower_password
+            unique_case += 1
+            combined += lcl
+        if up_case:
+            upper_password = random.choice(ucl)
+            initial_password += upper_password
+            unique_case += 1
+            combined += ucl
         if a_number:
-            num_password = upper_password.replace(random.choice(upper_password), random.choice(num))
-            print(num_password)
-            if a_char:
-                char_password = num_password.replace(random.choice(num_password), random.choice(char))
-                print(char_password)
-            else:
-                print('no char')
-        else:
-            print('no number')
-
-    if a_number:
-        num_password = initial_password.replace(random.choice(initial_password), random.choice(num))
-        print(num_password)
+            num_password = random.choice(num)
+            initial_password += num_password
+            unique_case += 1
+            combined += num
         if a_char:
-            char_password = num_password.replace(random.choice(num_password), random.choice(char))
-            print(char_password)
-        else:
-            print('no char')
-
-    
-    # else:
-    #     print(initial_password)  
-
-    if a_number:
-        print("a number from here")
-    #     num_password = initial_password.replace(random.choice(initial_password), random.choice(num))
-    #     print(num_password)
-    # else:
-    #     print(initial_password)
-
-
-    #print(password)    
-
-    # random.shuffle(password)
-    #print('You have generated the password:', "".join(password))
+            char_password = random.choice(char)
+            initial_password += char_password
+            unique_case += 1
+            combined += char
+      
+        for _ in range(pass_length_num - unique_case):
+            remaining_password += random.choice(combined)
+        
+        temp_password = ''.join(initial_password + remaining_password)
+        new_passowrd = ''.join(random.sample(temp_password,len(temp_password)))
+        print('-' * 22)
+        print('GENERATED PASSWORD')
+        print('-' * 22)
+        print('[+] Your new password is:', new_passowrd)
+        print('-' * 22)
 
 
 if __name__ == "__main__":
